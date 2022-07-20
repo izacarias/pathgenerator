dea369c341a424806e03e259ed863185e17d7187
izacarias <iuli.zacarias@gmail.com>
2022-07-20 19:19:10 +0200

    Implementing K shortest paths

--- src/main/java/placement/App.java
+++ src/main/java/placement/App.java
@@ -22,17 +22,42 @@ public class App
         // Set the graph filename 
         String fileName = "./src/main/resources/topologies/7nodes.dgs";
         Graph graph = GraphLoader.loadGraph(fileName);
+
+        // Setting weights for Shortest Path calculation and labels for visualizations
+        graph.edges().forEach(e -> e.setAttribute("weight", 100));
+        graph.nodes().forEach(n -> n.setAttribute("label", n.getId()));
+        graph.edges().forEach(e -> e.setAttribute("label", "" + (int) e.getAttribute("weight")));
+
         PathFinder pf = new PathFinder(graph);
-        pf.computeAllPaths();
-        pf.printAllPaths();
-        // Finding paths
+        pf.computeKShortestPaths(3);
 
 
         // Displaying the Graph
-        graph.setAttribute("ui.stylesheet", "node { fill-color: blue; text-size: 20; text-background-mode: plain;}");
-        for (Node node : graph) {
-            node.setAttribute("ui.label", node.getId());
-        }
+        // showGraph(graph);
+    }
+
+    /**
+     * Displays the graph using the native Graphstream vistualization tool basen on
+     * Java Swing.
+     * @param graph
+     */
+    private static void showGraph(Graph graph) {
+        String css = "";
+        css = "node { " +
+                "fill-mode: none; " +
+                "stroke-mode: plain;" + 
+                "stroke-color: #5585b5;" + 
+                "stroke-width: 3;" +
+                "text-size: 12; " +
+                "text-background-mode: rounded-box; " +
+                "text-background-color: #bbe4e9;"+
+                "text-padding: 5px, 4px; " + 
+                "text-offset: 0px, 5px;" +
+                "text-alignment: under;" +
+            "}";
+        // setting CSS Style
+        graph.setAttribute("ui.stylesheet", css);
+        // configuring the rendering engine to swing
         System.setProperty("org.graphstream.ui", "swing");
         graph.display();
     }
--- src/main/java/placement/core/PathFinder.java
+++ src/main/java/placement/core/PathFinder.java
@@ -1,15 +1,16 @@
 package placement.core;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Stack;
+
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
+import org.graphstream.graph.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class PathFinder {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(PathFinder.class);
     private Graph graph;
 
@@ -17,40 +18,25 @@ public class PathFinder {
         this.graph = graph;
     }
 
-    public void computeAllPaths() {
-        Node srcNode, dstNode;
-        // for (int i = 0; i < graph.getNodeCount(); i++) {
-        //     for (int j = 0; j < graph.getNodeCount(); j++) {
-        //         srcNode = graph.getNode(i);
-        //         dstNode = graph.getNode(j);
-        //         this.findPaths(srcNode, dstNode);
-        //     }
-        // }
-        srcNode = graph.getNode(0);
-        dstNode = graph.getNode(3);
-        this.findPaths(srcNode, dstNode, 5);
+    public void computeKShortestPaths(int K) {
+        logger.debug("Computing {} shortest paths", K);
+        ArrayList<Path> paths = new ArrayList<>();
+        graph.nodes().forEach(srcNode -> {
+            graph.nodes().forEach(dstNode -> {
+                if (!srcNode.equals(dstNode)){
+                    generatePaths(srcNode, dstNode);
+                }
+            });
+        });
     }
 
-    private void findPaths(Node srcNode, Node dstNode, int limit) {
-        // if (limit == 0)
-        //     return;
-        // ArrayList<Node> path = new ArrayList<>();
-        // if (! srcNode.equals(dstNode)) {
-        //     Iterator<? extends Node> k = srcNode.getDepthFirstIterator();
-        //     while (k.hasNext()) {
-        //         Node nextK = k.next();
-        //         path.add(nextK);
-        //         if (nextK.equals(dstNode)) {
-        //             System.out.println(path);
-        //             return;
-        //         }
-        //         findPaths(nextK, dstNode, limit -1);
-        //     }
-        // }
+    private void generatePaths(Node srcNode, Node dstNode) {
+        logger.debug("Generating paths from {} to {}", srcNode.getId(), dstNode.getId());
+
+        Stack<Node> pathNodes = new Stack<Node>();
+        ArrayList<Node> onPath = new ArrayList<Node>();
+        // Iterator<? extends Node> k = srcNode.;
+        srcNode.getNe
     }
 
-    public void printAllPaths() {
-        logger.info("Operation not supported yet.");
-    }
-    
 }
